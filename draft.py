c6000152551b1d0cc781d4b1c9ea59d1b8e6ea20
tanshengzhe <tanshengzhe@users.noreply.github.com>
2017-10-05 09:52:07 +0800

    tsz
    first change

---
+++ draft.py
@@ -0,0 +1,17 @@
+class User():
+    def __init__(self,name,password):
+        self.name=name
+        self.password=password
+        self.value=0
+    def increament_value(self,ppp):
+        self.valuppp
+    def reset(self,ddd=1):
+        self.value=0
+    def zhiding(self,zhi):
+        self.value=zhi
+tony=User('ss','ff')
+User.increament_value()
+User.increament_value(1)
+User.increament_value(1)
+User.increament_value(1)
+User.zhiding()
